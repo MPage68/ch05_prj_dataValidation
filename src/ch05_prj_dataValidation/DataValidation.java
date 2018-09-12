6d9eb56f17568cf218beee2e1c63a7bf1a6f338c
mpage68 <mikepage68@live.com>
2018-09-12 08:00:16 -0400

    update

--- src/ch05_prj_dataValidation/DataValidation.java
+++ src/ch05_prj_dataValidation/DataValidation.java
@@ -20,57 +20,21 @@ public class DataValidation {
 		while (choice.equalsIgnoreCase("y")) {
 
 			// user input
-			length = getDouble(sc, "Enter length:\t");
-			width = getDouble(sc, "Enter width:\t");
-			// logic
+			System.out.println("Enter length:\t");
+			length = sc.nextDouble();
+			System.out.println("Enter width:\t");
+			width = sc.nextDouble();
 			area = (length * width);
 			perimeter = (2 * width + 2 * length);
 			// user output
 			System.out.println("Area:\t        " + area + "\n" + "Perimeter:\t" + perimeter);
 
-			choice = getString(sc, "Continue:\t (y/n)\t");
+			choice = Console.getString("Continue(y/n)?", "y", "n");
+			;
 		}
 		// Goodbye statement/scanner close
 		System.out.println("Goodbye");
 		sc.close();
-
 	}
 
-	public static double getDouble(Scanner sc, String prompt) {
-		double d = 0;
-		boolean isValid = false;
-		while (!isValid) {
-			System.out.println(prompt);
-			if (sc.hasNextDouble()) {
-				d = sc.nextDouble();
-				isValid = true;
-			} else {
-				System.out.println("Error!");
-			}
-			sc.nextLine();
-		}
-		return d;
-	}
-
-	public static String getString(Scanner sc, String prompt) {
-		String d = "";
-		boolean isValid = false;
-		while (!isValid) {
-			System.out.println(prompt);
-			if (sc.hasNext()) {
-				d = sc.next();
-				isValid = true;
-			}
-			if (d.equalsIgnoreCase("y")) {
-				isValid = true;
-			}
-			if (d.equalsIgnoreCase("n")) {
-				isValid = true;
-			} else {
-				System.out.println("Error!");
-			}
-			sc.nextLine();
-		}
-		return d;
-	}
 }
